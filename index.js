66a5721fff338ee9f55cf574258e793fd0b477fc
Do Ho <hsnd_1739@q.com>
2022-04-01 13:17:14 -0600

    new changes to index.js

--- index.js
+++ index.js
@@ -23,3 +23,44 @@ console.log(5 == 6);
 console.log(bankAccountBalance == numberOfFriends);
 console.log(bankAccountBalance == 52.10);
 console.log(numberOfFriends == 4);
+
+var x = 35;
+var y = "20";
+var z = x + y;
+console.log(z); //when you print this you will get 3520 but the value 
+// of z is "3520" because you are concantenating the two variables
+
+
+var num1 = 42;
+var num2 = 10;
+
+//write your code here
+console.log((num1 - num2) * 16 );
+
+var x = 8;
+var y = 12;
+x = y + x
+var z = x + y;
+console.log(z);
+
+var name = "Doreen";
+
+for (var i = 0; i < 10; i++){
+    for (var j = 0; j < 3; j++)
+    console.log(name)} //this prints out Doreen 30 times; in this loop the conditions are met to be true in the first 'for' statement,
+    //so Doreen would print 10 times but the conditions are met in the second 'for' statement which makes the first 'for' statement repeat
+    //3 times; so basically 10 'Doreen's will print 3 times to make a total of 30 times.
+
+var x = 5;
+var y = 6;
+x = 3 +y
+var z = x + y;
+y = 5 + 2*x
+x = y
+console.log(z);
+
+var a;
+var b = 38;
+var c = a + b;
+console.log(c); //this prints out a NaN because it's a failed math problem
+
