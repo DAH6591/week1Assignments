710db841ac0345c2da2869fe9deebc1ddf2a47cd
Do Ho <hsnd_1739@q.com>
2022-04-04 18:34:01 -0600

    commit changes to index.js

--- index.js
+++ index.js
@@ -43,11 +43,13 @@ x = y + x
 var z = x + y;
 console.log(z);
 
-var name = "Doreen";
+
+
+var Name = "Doreen";
 
 for (var i = 0; i < 10; i++){
     for (var j = 0; j < 3; j++)
-    console.log(name)} //this prints out Doreen 30 times; in this loop the conditions are met to be true in the first 'for' statement,
+    console.log(Name)} //this prints out Doreen 30 times; in this loop the conditions are met to be true in the first 'for' statement,
     //so Doreen would print 10 times but the conditions are met in the second 'for' statement which makes the first 'for' statement repeat
     //3 times; so basically 10 'Doreen's will print 3 times to make a total of 30 times.
 
@@ -63,4 +65,83 @@ var a;
 var b = 38;
 var c = a + b;
 console.log(c); //this prints out a NaN because it's a failed math problem
+
+var m = 37;
+console.log(m / 0);
+
+console.log(0 / 0);
+
+console.log(-37 / 0);
+
+var lastName = "Hosk";
+var age = 65;
+
+var person = {
+    lastName: "Hosk",
+    age: 65
+};
+console.log(person);
+
+//Dot Notation
+person.lastName = "Hoskins";
+console.log(person);
+console.log(person.lastName);
+
+//Bracket Notation
+person["lastName"] = "Smith";
+console.log(person.lastName);
+
+//arrays
+//let selectedColors = []; This is an empty array.
+let selectedColors = ['red', 'blue'];  //red and blue are elements
+console.log(selectedColors);
+console.log(selectedColors[0]); //This prints only red
+
+//Lengths of arrays and types of objects in an array can change because they are Dynamic
+selectedColors[2] = 'green'; //we're  ll see (3) ['red', 'blue', 'green']
+
+//can change the type of one of the objects in an array
+selectedColors[2] = 1;
+console.log(selectedColors);
+
+console.log(selectedColors.length);
+
+//functions
+//function declaration: curly brackets
+//body of function: what's inside the curly brackets; statements that define logic in our application
+//Examples below to line 132 are for Performing a Task
+function greet() {
+    console.log('Hello World');
+}
+
+//calling the function
+greet(); //calling the function; need this to actually see Hello World in the console
+
+//adding parameters and arguments to functions
+function greeting (nickname){ //nickname is a parameter; a variable that is only meaningful inside the function
+    console.log('Hello ' + nickname);
+}
+greeting('Dora'); //Dora is an argument to the greeting function; the actual value we supply for the nickname parameter
+greeting('goofball');
+
+//a function can have multipe parameters
+function greeting (nickname, maidenName) {
+    console.log("Hello " + nickname + ' ' + maidenName);
+}
+greeting('Dora', 'Hoskins'); //if you forget to add a value here to the added variable or parameter maidenName
+//you will return an undefined in the console for that variable but Dora will show up
+
+//example of function that calculates a value
+function square(number) {
+    return number * number;   
+}
+let number = square(2);
+console.log(number);
+
+//don't have to assign square(2) to the variable number like line 138
+function square(number) {
+    return number * number;
+}
+console.log(square(2)); //there are two function calls: square(2) and also console.log  Calling the log function and passing an expression,
+//the expression can be a call to another function like square(2)
 
